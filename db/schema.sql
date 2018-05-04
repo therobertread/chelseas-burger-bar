b0accf8ce33d7d95c7ade298d49627067ddf6be1
therobertread <38984551+therobertread@users.noreply.github.com>
2018-05-04 13:39:09 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -15,17 +15,6 @@ CREATE TABLE burgers(
   id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(64) NOT NULL,
   devoured BOOLEAN NOT NULL DEFAULT 0,
-  date TIMESTAMP NOT NULL, DEFAULT '2038-01-19 03:14:07'
-  PRIMARY KEY(id)
-);
-/* Create a table called 'menu' with the following columns
-	id - auto incrementing 'int' that serves as a primary key
-	item - a string - make it 64 characters long
-	entree - a boolean defaulted to '1'
-*/
-CREATE TABLE menu(
-  id INT AUTO_INCREMENT NOT NULL,
-  item VARCHAR(64) NOT NULL,
-  entree BOOLEAN NOT NULL DEFAULT 1,
+  date TIMESTAMP,
   PRIMARY KEY(id)
 );
