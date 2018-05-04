7988d3fdc29ab4f95addcce85e51660d99343410
therobertread <38984551+therobertread@users.noreply.github.com>
2018-05-04 13:42:30 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,36 +1,25 @@
 /*
-*/
-/* Now open item*/
 USE burgers_db;
-
-INSERT INTO burgers (burger_name) VALUES ('Chelsea Market Burger');
-INSERT INTO burgers (burger_name) VALUES ('Skyler Special Burger');
-INSERT INTO burgers (burger_name) VALUES ('Roman Noodle Burger');
-INSERT INTO burgers (burger_name) VALUES ('What The Heck Burger');
-INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Bacon Burger');
+*/
+INSERT INTO burgers (burger_name) VALUES ('Chelsea Market Burger',0);
+INSERT INTO burgers (burger_name) VALUES ('Skyler Special Burger',0);
+INSERT INTO burgers (burger_name) VALUES ('Roman Noodle Burger',0);
+INSERT INTO burgers (burger_name) VALUES ('What The Heck Burger',0);
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Bacon Burger',0);
 /* Create a bunch of menu items
 */
-INSERT INTO menu (item) VALUES ('Mushroom Swiss Bacon Burger');
-INSERT INTO menu (item) VALUES ('What The Heck Burger');
-INSERT INTO menu (item) VALUES ('Roman Noodle Burger');
-INSERT INTO menu (item) VALUES ('Skyler Special Burger');
-INSERT INTO menu (item) VALUES ('Chelsea Market Burger');
-INSERT INTO menu (item) VALUES ('Buca Veggie Burger');
-INSERT INTO menu (item) VALUES ('Surprise Sink Burger');
-INSERT INTO menu (item) VALUES ('In Your Face Burger');
-INSERT INTO menu (item) VALUES ('Chef Special Burger');
-INSERT INTO menu (item) VALUES ('Pizza Burger');
-INSERT INTO menu (item) VALUES ('Wimpy Burger');
-INSERT INTO menu (item) VALUES ('Zesty Italian Burger');
-INSERT INTO menu (item) VALUES ('Bacon Burger');
-INSERT INTO menu (item) VALUES ('Spicy Taco Burger');
-INSERT INTO menu (item) VALUES ('Double Cheese Bacon Bacon Burger');
-/* 
-*/
-INSERT INTO menu (item, entree) VALUES ('Bread Crusts with Mayo', 0);
-INSERT INTO menu (item, entree) VALUES ('Putun Fries', 0);
-INSERT INTO menu (item, entree) VALUES ('Caesar Salid', 0);
-INSERT INTO menu (item, entree) VALUES ('Chef Salad', 0);
-INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
-INSERT INTO menu (item, entree) VALUES ('Rostie Potatoes', 0);
-INSERT INTO menu (item, entree) VALUES ('Texas Garlic Toast', 0);
+INSERT INTO menu (item) VALUES ('Mushroom Swiss Bacon Burger',0);
+INSERT INTO menu (item) VALUES ('What The Heck Burger',0);
+INSERT INTO menu (item) VALUES ('Roman Noodle Burger',0);
+INSERT INTO menu (item) VALUES ('Skyler Special Burger',0);
+INSERT INTO menu (item) VALUES ('Chelsea Market Burger',0);
+INSERT INTO menu (item) VALUES ('Buca Veggie Burger',0);
+INSERT INTO menu (item) VALUES ('Surprise Sink Burger',0);
+INSERT INTO menu (item) VALUES ('In Your Face Burger',0);
+INSERT INTO menu (item) VALUES ('Chef Special Burger',0);
+INSERT INTO menu (item) VALUES ('Pizza Burger',0);
+INSERT INTO menu (item) VALUES ('Wimpy Burger',0);
+INSERT INTO menu (item) VALUES ('Zesty Italian Burger',0);
+INSERT INTO menu (item) VALUES ('Bacon Burger',0);
+INSERT INTO menu (item) VALUES ('Spicy Taco Burger',0);
+INSERT INTO menu (item) VALUES ('Double Cheese Bacon Bacon Burger',0);
